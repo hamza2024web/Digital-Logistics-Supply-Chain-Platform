bd8adb0da475355b775e1a0ed57fea036e5e46cd
hamza2024web <hamzaakroubi1528@gmail.com>
2025-10-28 12:05:58 +0100

    create ware house entity

---
+++ src/main/java/com/spring/digital_logistics/entity/Warehouse.java
@@ -0,0 +1,24 @@
+package com.spring.digital_logistics.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "warehouse")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Warehouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false , unique = true)
+    private String code;
+
+    @Column(nullable = false)
+    private String name;
+}
