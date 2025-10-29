6ad0e34a412e97a0d347aea0dfae6165b9dcdb49
hamza2024web <hamzaakroubi1528@gmail.com>
2025-10-29 23:47:29 +0100

    create the custom excption and controller advice to centralize the exceptions

---
+++ src/main/java/com/spring/digital_logistics/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.spring.digital_logistics.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public class GlobalExceptionHandler{
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAll(Exception ex){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erreur interne : " + ex.getMessage());
+    }
+}
