aa40bfe9a7207c601873f452886d42aa64b5d134
hamza2024web <hamzaakroubi1528@gmail.com>
2025-10-30 10:03:47 +0100

    create the controller of register and the methode to create the user

---
+++ src/main/java/com/spring/digital_logistics/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.spring.digital_logistics.controller;
+
+import com.spring.digital_logistics.dto.UserCreateDTO;
+import com.spring.digital_logistics.dto.UserDTO;
+import com.spring.digital_logistics.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth/register")
+public class AuthController {
+    private final UserService userService;
+
+    public AuthController (UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> register(@RequestBody UserCreateDTO userCreateDTO){
+        UserDTO createUser = userService.createUser(userCreateDTO);
+        return ResponseEntity.ok(createUser);
+    }
+}
