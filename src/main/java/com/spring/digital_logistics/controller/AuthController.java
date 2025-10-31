e6b2a8d2b301e9f108e216c94560a6d736c01b3c
hamza2024web <hamzaakroubi1528@gmail.com>
2025-10-31 12:29:14 +0100

    DL-8 create the class config for spring security in order to work with swagger

--- src/main/java/com/spring/digital_logistics/controller/AuthController.java
+++ src/main/java/com/spring/digital_logistics/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.spring.digital_logistics.controller;
 import com.spring.digital_logistics.dto.request.UserCreateDTO;
 import com.spring.digital_logistics.dto.response.UserDTO;
 import com.spring.digital_logistics.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,8 +20,9 @@ public class AuthController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Inscritpion d'un nouvel utilisateur ")
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> register(@RequestBody UserCreateDTO userCreateDTO){
+    public ResponseEntity<UserDTO> register(@Valid @RequestBody UserCreateDTO userCreateDTO){
         UserDTO createUser = userService.register(userCreateDTO);
         return ResponseEntity.ok(createUser);
     }
