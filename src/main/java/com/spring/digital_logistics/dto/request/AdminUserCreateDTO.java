a04d9ca41e6918fc7493efe9d00dff5dc1011af1
hamza2024web <hamzaakroubi1528@gmail.com>
2025-11-03 00:20:30 +0100

    DL-27 create the dto of admin create user

---
+++ src/main/java/com/spring/digital_logistics/dto/request/AdminUserCreateDTO.java
@@ -0,0 +1,28 @@
+package com.spring.digital_logistics.dto.request;
+
+import com.spring.digital_logistics.entity.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class AdminUserCreateDTO {
+    @NotBlank(message = "Le prénom est obligatoire")
+    private String firstName;
+
+    @NotBlank(message = "Le nom est obligatoire")
+    private String lastName;
+
+    @Email(message = "L'email doit être valide")
+    @NotBlank(message = "L'email est obligatoire")
+    private String email;
+
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    @Size(min = 8, message = "Le mot de passe doit contenir au moins 8 caractères")
+    private String password;
+
+    @NotNull(message = "Le rôle est obligatoire")
+    private Role role;
+}
