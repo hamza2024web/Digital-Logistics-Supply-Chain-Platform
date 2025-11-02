b0aaeac5b1137c93ab0d802f908e3e24110859de
hamza2024web <hamzaakroubi1528@gmail.com>
2025-11-02 20:09:26 +0100

    DL-27 create the methode hello to verify is the token jwt work correctly

---
+++ src/main/java/com/spring/digital_logistics/controller/AdminController.java
@@ -0,0 +1,18 @@
+package com.spring.digital_logistics.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    @GetMapping("/hello")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<String> sayHelloToAdmin(){
+        return ResponseEntity.ok("Bonjour, Administrateur ! Si vous voyez ce message, votre token est valide et vous avez le bon rôle.");
+    }
+}
