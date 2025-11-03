12bf61cd371a37ee771952deb6f80ffe0fa6ba13
hamza2024web <hamzaakroubi1528@gmail.com>
2025-11-03 11:01:46 +0100

    DL-36 create the dto (response , request ) of ware house

---
+++ src/main/java/com/spring/digital_logistics/dto/request/WarehouseCreateDTO.java
@@ -0,0 +1,12 @@
+package com.spring.digital_logistics.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class WarehouseCreateDTO {
+
+    @NotBlank(message = "Le code de l'entrepôt ne doit pas étre vide")
+    private String code;
+
+    @NotBlank(message = "Le nom de l'entrepôt ne doit pas étre vide")
+    private String name;
+}
---
+++ src/main/java/com/spring/digital_logistics/dto/response/WarehouseDTO.java
@@ -0,0 +1,7 @@
+package com.spring.digital_logistics.dto.response;
+
+public class WarehouseDTO {
+    private Long id;
+    private String code;
+    private String name;
+}
