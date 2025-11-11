20a076d05bbe6b72c6b0da32dd86890d5368d7f1
hamza2024web <hamzaakroubi1528@gmail.com>
2025-11-11 13:47:35 +0100

    DL-112 create the interface of transfer order

---
+++ src/main/java/com/spring/digital_logistics/repository/TransferOrderRepository.java
@@ -0,0 +1,10 @@
+package com.spring.digital_logistics.repository;
+
+import com.spring.digital_logistics.entity.TransferOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransferOrderRepository extends JpaRepository<TransferOrder,Long> {
+
+}
