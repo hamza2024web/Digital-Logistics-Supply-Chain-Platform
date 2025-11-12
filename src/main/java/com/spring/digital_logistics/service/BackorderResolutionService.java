1d882f43d3b87f19f543de8bf3e753adc9d20c12
hamza2024web <hamzaakroubi1528@gmail.com>
2025-11-12 14:51:32 +0100

    DL-112 add a private function when a sales order line updated to a status reserved the sales order parent should also be updated , so the new private function do that

--- src/main/java/com/spring/digital_logistics/service/BackorderResolutionService.java
+++ src/main/java/com/spring/digital_logistics/service/BackorderResolutionService.java
@@ -2,12 +2,10 @@ package com.spring.digital_logistics.service;
 
 import com.spring.digital_logistics.entity.*;
 import com.spring.digital_logistics.entity.enums.SalesOrderLineStatus;
+import com.spring.digital_logistics.entity.enums.SalesOrderStatus;
 import com.spring.digital_logistics.entity.enums.TransferStatus;
 import com.spring.digital_logistics.exception.ResourceNotFoundException;
-import com.spring.digital_logistics.repository.InventoryRepository;
-import com.spring.digital_logistics.repository.SalesOrderLineRepository;
-import com.spring.digital_logistics.repository.TransferOrderRepository;
-import com.spring.digital_logistics.repository.WarehouseRepository;
+import com.spring.digital_logistics.repository.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -25,14 +23,16 @@ public class BackorderResolutionService {
     private final WarehouseRepository warehouseRepository;
     private final TransferOrderRepository transferOrderRepository;
     private final InventoryService inventoryService;
+    private final SalesOrderRepository salesOrderRepository;
     private static final Logger log = LoggerFactory.getLogger(SupplierSimulationService.class);
 
-    public BackorderResolutionService(SalesOrderLineRepository salesOrderLineRepository, InventoryRepository inventoryRepository, WarehouseRepository warehouseRepository, TransferOrderRepository transferOrderRepository, InventoryService inventoryService) {
+    public BackorderResolutionService(SalesOrderLineRepository salesOrderLineRepository, InventoryRepository inventoryRepository, WarehouseRepository warehouseRepository, TransferOrderRepository transferOrderRepository, InventoryService inventoryService, SalesOrderRepository salesOrderRepository) {
         this.salesOrderLineRepository = salesOrderLineRepository;
         this.inventoryRepository = inventoryRepository;
         this.warehouseRepository = warehouseRepository;
         this.transferOrderRepository = transferOrderRepository;
         this.inventoryService = inventoryService;
+        this.salesOrderRepository = salesOrderRepository;
     }
 
     @Scheduled(fixedRate = 6000)
@@ -68,37 +68,34 @@ public class BackorderResolutionService {
 
                 line.setStatus(SalesOrderLineStatus.RESERVED);
                 salesOrderLineRepository.save(line);
+
+                checkAndReserveParentOrder(line.getSalesOrder());
             }
         }
     }
 
-    private void resolveBackorderedLines(){
-        List<SalesOrderLine> backorderLines = salesOrderLineRepository.findByStatus(SalesOrderLineStatus.BACKORDERED);
-        if (backorderLines.isEmpty()){
+    private void resolveBackorderedLines() {
+        List<SalesOrderLine> backorderedLines = salesOrderLineRepository.findByStatus(SalesOrderLineStatus.BACKORDERED);
+        if (backorderedLines.isEmpty()) {
             return;
         }
 
-        log.info("[AGENT] {} lignes en 'BACKORDERED' trouvées. Recherche de solutions de transfert...",backorderLines.size());
-        for(SalesOrderLine line : backorderLines){
+        log.info("[AGENT] {} lignes en 'BACKORDERED' trouvées. Recherche de solutions de transfert...", backorderedLines.size());
+        for (SalesOrderLine line : backorderedLines) {
             Product productToFind = line.getProduct();
             Warehouse destinationWarehouse = line.getSalesOrder().getWarehouse();
             int quantityNeeded = line.getQuantity();
-
-            boolean transferAlreadyPending = transferOrderRepository.existsByProductAndDestinationWarehouseAndStatus(productToFind,destinationWarehouse,TransferStatus.PENDING);
-            if (transferAlreadyPending) {
-                continue;
-            }
 
             List<Warehouse> otherWarehouses = warehouseRepository.findAll().stream()
                     .filter(w -> !w.getId().equals(destinationWarehouse.getId()))
                     .toList();
 
-            for (Warehouse sourceWarehouse : otherWarehouses){
-                Inventory remoteInventory = inventoryRepository.findByProductAndWarehouse(productToFind,sourceWarehouse).orElse(new Inventory());
+            for (Warehouse sourceWarehouse : otherWarehouses) {
+                Inventory remoteInventory = inventoryRepository.findByProductAndWarehouse(productToFind, sourceWarehouse).orElse(new Inventory());
                 int remoteAvailableStock = remoteInventory.getQtyOnHand() - remoteInventory.getQtyReserved();
 
-                if (remoteAvailableStock >= quantityNeeded){
-                    log.info("   -> [SOLUTION] Stock trouvé pour SKU {} à {}. Création d'un ordre de transfert.", productToFind.getSku() , sourceWarehouse.getCode());
+                if (remoteAvailableStock >= quantityNeeded) {
+                    log.info("   -> [SOLUTION] Stock trouvé pour SKU {} à {}. Création d'un ordre de transfert.", productToFind.getSku(), sourceWarehouse.getCode());
 
                     TransferOrder transferOrder = new TransferOrder(productToFind, sourceWarehouse, destinationWarehouse, quantityNeeded, TransferStatus.PENDING, LocalDateTime.now());
                     transferOrderRepository.save(transferOrder);
@@ -109,6 +106,20 @@ public class BackorderResolutionService {
                     break;
                 }
             }
+        }
+    }
+
+    private void checkAndReserveParentOrder(SalesOrder salesOrder){
+        SalesOrder freshOrder = salesOrderRepository.findById(salesOrder.getId()).orElseThrow(() -> new IllegalStateException("Erreur critique: la commande parent n'a pas été trouvée."));
+
+        boolean allLinesAreReserved = freshOrder.getLines().stream()
+                .allMatch(line -> line.getStatus() == SalesOrderLineStatus.RESERVED);
+
+        if (allLinesAreReserved){
+            log.info("   -> [COMMANDE PRÊTE] Toutes les lignes de la commande #{} sont maintenant réservées. Mise à jour du statut global.", freshOrder.getId());
+
+            freshOrder.setStatus(SalesOrderStatus.RESERVED);
+            salesOrderRepository.save(freshOrder);
         }
     }
 }
