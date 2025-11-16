83769f3db09da8cb270f9cb0a757666bc573315c
hamza2024web <hamzaakroubi1528@gmail.com>
2025-11-16 17:10:25 +0100

    DL-113 fix(ci): Use the url offre by the test containers

--- src/test/java/com/spring/digital_logistics/IntegrationTestBase.java
+++ src/test/java/com/spring/digital_logistics/IntegrationTestBase.java
@@ -1,34 +1,36 @@
-// Fichier : src/main/java/com/spring/digital_logistics/IntegrationTestBase.java
 package com.spring.digital_logistics;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.Duration;
 
 @SpringBootTest
 @Testcontainers
 public abstract class IntegrationTestBase {
+
+    @Container
     static PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres:15-alpine")
-            .withNetworkAliases("postgres-for-test")
-            .withNetworkMode("jenkins-net");
+            .withDatabaseName("test")
+            .withUsername("test")
+            .withPassword("test")
+            .withStartupTimeout(Duration.ofMinutes(2))
+            .waitingFor(Wait.forLogMessage(".*database system is ready to accept connections.*\\n", 2));
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
-        database.start();
-
-        String jdbcUrl = String.format("jdbc:postgresql://postgres-for-test:%d/%s",
-                5432,
-                database.getDatabaseName());
-
-        registry.add("spring.datasource.url", () -> jdbcUrl);
+        registry.add("spring.datasource.url", database::getJdbcUrl);
         registry.add("spring.datasource.username", database::getUsername);
         registry.add("spring.datasource.password", database::getPassword);
 
-        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
-        registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.PostgreSQLDialect");
-        registry.add("jwt.secret", () -> "secret-key-for-integration-tests");
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+
+        registry.add("jwt.secret", () -> "une-fausse-cle-pour-les-tests-qui-fonctionne");
         registry.add("spring.docker.compose.enabled", () -> "false");
     }
 }
