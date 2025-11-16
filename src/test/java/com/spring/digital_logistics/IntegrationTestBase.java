a758712bbb9681383f1260b4f1d75902e9bacdc5
hamza2024web <hamzaakroubi1528@gmail.com>
2025-11-16 15:55:10 +0100

    DL-113 fix : add the time stamp to garanti the container of postgres is ready

--- src/test/java/com/spring/digital_logistics/IntegrationTestBase.java
+++ src/test/java/com/spring/digital_logistics/IntegrationTestBase.java
@@ -4,18 +4,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.Duration;
 
 @SpringBootTest
 @Testcontainers
 public abstract class IntegrationTestBase {
 
-    static PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres:15-alpine");
+    @Container
+    static PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("test")
+            .withUsername("test")
+            .withPassword("test")
+            .withStartupTimeout(Duration.ofMinutes(2))
+            .waitingFor(Wait.forListeningPort().withStartupTimeout(Duration.ofMinutes(2)));
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
-        database.start();
-
         registry.add("spring.datasource.url", database::getJdbcUrl);
         registry.add("spring.datasource.username", database::getUsername);
         registry.add("spring.datasource.password", database::getPassword);
