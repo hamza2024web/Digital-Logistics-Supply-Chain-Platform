03beca260b67e0960bd0e955e05ad9c8da82a655
hamza2024web <hamzaakroubi1528@gmail.com>
2025-12-24 09:03:47 +0100

    create the class user details impl

--- src/main/java/com/spring/digital_logistics/dto/response/login/LoginResponseDTO.java
+++ src/main/java/com/spring/digital_logistics/dto/response/login/LoginResponseDTO.java
@@ -1,10 +1,12 @@
 package com.spring.digital_logistics.dto.response.login;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class LoginResponseDTO {
     private String token;
     private String refreshToken;
---
+++ src/main/java/com/spring/digital_logistics/security/service/UserDetailsImpl.java
@@ -0,0 +1,97 @@
+package com.spring.digital_logistics.security.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.spring.digital_logistics.entity.User;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+@Data
+public class UserDetailsImpl implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String username;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImpl(Long id, String username, String email, String password,
+                           Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    // Cette méthode convertit votre Entité User -> UserDetailsImpl
+    public static UserDetailsImpl build(User user) {
+        // On suppose qu'un user a un rôle (ex: "ROLE_ADMIN")
+        // Adaptez "user.getRole()" selon comment vous stockez le rôle (String ou Entité)
+        List<GrantedAuthority> authorities = Collections.singletonList(
+                new SimpleGrantedAuthority(user.getRole().name())
+        );
+
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getEmail(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username; // ou email
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        UserDetailsImpl user = (UserDetailsImpl) o;
+        return Objects.equals(id, user.id);
+    }
+}
